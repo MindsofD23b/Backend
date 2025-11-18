02c4e963724e46e653bb667110997f8ca8339521
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-11-18 14:34:43 +0100

    [init] init linting and testing for backend

---
+++ srv/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { config } from 'dotenv'
+config();
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send("<h1>Hello World!</h1>"))
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
